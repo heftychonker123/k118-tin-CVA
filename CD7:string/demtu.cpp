82e09623c2a32e267a98ae6318fab7a42bb6b49c
heftychonker123 <215478143+heftychonker123@users.noreply.github.com>
2025-10-24 16:50:34 +0700

    24/10/2025

---
+++ CD7:string/demtu.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);r
+    cin.tie(nullptr);
+
+    string n;
+    getline(cin, n);
+
+    stringstream ss(n);
+    vector<string> s((istream_iterator<string>(ss)), istream_iterator<string>());
+
+    cout << s.size() << '\n';
+    for (const string &word : s) {
+        cout << word << '\n';
+    }
+
+    return 0;
+}
