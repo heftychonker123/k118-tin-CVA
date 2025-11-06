fd49034e9191b0e18e534ae7eee6abd2175c2f16
heftychonker123 <215478143+heftychonker123@users.noreply.github.com>
2025-11-06 15:40:59 +0700

    6/11/2025

--- cleanupp/cleanup.cpp
+++ cleanupp/cleanup.cpp
@@ -3,13 +3,10 @@
 int main() {
     // Execute the Bash script
     int result1 = system("./cleanup.sh");
+    if (result1!=0) std::cout << "Cleanup.sh executed unsuccessfully with return code " << result1;
     int result2 = system("./standardize.sh");
-    // Check the result
-    if (result1 == 0 && result2 == 0) {
-        std::cout << "Script executed successfully.\n";
-    } else {
-        std::cerr << "Script execution failed with code " << result1 << "\n" << result2 << "\n";
-    }
-
+    if (result2!=0) std::cout << "Standardize.sh executed unsuccessfully with return code " << result2;
+    int result3 = system("./gitpush.sh");
+    if (result3!=0) std::cout << "Git-push executed unsuccessfully with return code " << result3;
     return 0;
 }
--- cleanupp/cleanup.sh
+++ cleanupp/cleanup.sh
@@ -18,12 +18,3 @@ for file in build/*.cpp; do
     mv "$file" "$prefix/"
 done
 
-# Clean up the build directory
-rm -rf build
-mkdir build
-
-# Git operations
-read -p "Commit message: " msg
-git add .
-git commit -m "$msg"
-git push origin main
